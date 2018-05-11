a87892d35dee32bc4e7de0cef61f4c7c4d64fbb4
nbb0101 <18735841472@163.com>
2018-05-11 09:15:34 +0800

    ****

--- src/js/change.js
+++ src/js/change.js
@@ -1,8 +1,12 @@
 function change(num) {
     var num = num.toString();
 
-    console.log(num);
+    var str = num.substring(14);
+    str = '****';
+    var strs = num.substring(0, 14) + str;
+    return strs;
 }
 //142327199401010630
 
-change(142327199401010630);
+
+console.log(change(142327199401010630));
